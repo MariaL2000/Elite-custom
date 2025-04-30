1cb58ced9d239c5c09b013bd3cba3d19eda6fe6f
MariaL2000 <mariamarreromedrano@gmail.com>
2025-04-30 09:55:22 -0700

    update

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -3,7 +3,7 @@ import { ReviewForm } from '@/components/review/ReviewForm';
 export default function ReviewPage() {
   return (
     <div className="flex justify-center py-8 xl:py-[2vw]">
-      <ReviewForm />;
+      <ReviewForm />
     </div>
   );
 }
