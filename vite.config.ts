86ae582476f723c1653e8a207f5403d68b357605
Geovani <geovanispartaco@gmail.com>
2025-05-31 18:17:27 -0400

    Certificates deleted

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import fs from 'fs';
-// import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,32 +12,4 @@ export default defineConfig({
     },
   },
   base: '/Elite-custom/',
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(
-        path.resolve(__dirname, '../Backendelite/main_project/certificates/key.pem')
-      ),
-      cert: fs.readFileSync(
-        path.resolve(__dirname, '../Backendelite/main_project/certificates/cert.pem')
-      ),
-    },
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:8001',
-        changeOrigin: true,
-        secure: false, // Ignora errores de certificado en desarrollo
-        rewrite: path => path.replace(/^\/api/, '/api'), // Keep the /api prefix
-      },
-      '/media': {
-        target: 'https://localhost:8001',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace(/^\/media/, '/media'),
-      },
-    },
-  },
 });
