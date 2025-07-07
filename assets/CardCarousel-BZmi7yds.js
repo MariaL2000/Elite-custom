689a9a5a294e2ce2d8aa8694ddafdc54888967cc
Geovani Franco <geovanispartaco@gmail.com>
2025-07-07 11:24:22 -0700

    Updates

--- assets/CardCarousel-BZmi7yds.js
+++ assets/CardCarousel-BZmi7yds.js
@@ -1,4 +1,4 @@
-import{c as x,r as l,j as t,S as y,A as h,m as a}from"./index-CGx1dWWv.js";/**
+import{c as x,r as l,j as t,S as y,A as h,m as a}from"./index-BKC7uZn4.js";/**
  * @license lucide-react v0.488.0 - ISC
  *
  * This source code is licensed under the ISC license.
