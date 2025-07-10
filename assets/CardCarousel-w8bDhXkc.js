366e0dde911de97f61223a2298f501b88aea240e
MariaL2000 <mariamarreromedrano@gmail.com>
2025-07-10 20:30:44 +0200

    Updates

--- assets/CardCarousel-w8bDhXkc.js
+++ assets/CardCarousel-w8bDhXkc.js
@@ -1,4 +1,4 @@
-import{c as x,r as l,j as t,S as y,A as h,m as a}from"./index-BiZXBFSh.js";/**
+import{c as x,r as l,j as t,S as y,A as h,m as a}from"./index-BqHxxMBK.js";/**
  * @license lucide-react v0.488.0 - ISC
  *
  * This source code is licensed under the ISC license.
