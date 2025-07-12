d37610b58d8aaee1b6be22c2e70d34a59d64ecd2
MariaL2000 <mariamarreromedrano@gmail.com>
2025-07-12 18:55:42 +0200

    Updates

--- assets/CardCarousel-DTpO3Gz2.js
+++ assets/CardCarousel-DTpO3Gz2.js
@@ -1,4 +1,4 @@
-import{c as x,r as l,j as t,S as h,A as y,m as a}from"./index-ImTtbr-j.js";/**
+import{c as x,r as l,j as t,S as h,A as y,m as a}from"./index-DLkzFSxR.js";/**
  * @license lucide-react v0.488.0 - ISC
  *
  * This source code is licensed under the ISC license.
